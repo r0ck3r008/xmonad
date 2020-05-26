25d8057d830f42a760a5b10be5dc95d54de30072
Naman Arora <arora.naman@protonmail.com>
2020-05-26 18:31:46 -0400

    moved away from i3lock to slock from suckless

--- xmonad.hs
+++ xmonad.hs
@@ -42,7 +42,7 @@ main=do
 		((mod4Mask, xK_Return), spawn "alacritty"),
 		((mod4Mask, xK_d), spawn "rofi -show drun -lines 5 -eh 2 -width 50 -padding 800 -bw 0 -threads 0 -theme Arc-Dark -show-icons -icon-theme Arc"),
 		((mod4Mask, xK_w), spawn "rofi -show window -lines 5 -eh 2 -width 50 -padding 800 -bw 0 -threads 0 -theme Arc-Dark -show-icons -icon-theme Arc"),
-		((mod1Mask, xK_l), spawn "i3lock -c 000000 -i ~/.xmonad/lock.png"),
+		((mod1Mask, xK_l), spawn "slock"),
 		((mod1Mask .|. controlMask, xK_s), spawn "systemctl poweroff"),
 		((mod1Mask, xK_d), spawn "trayer --edge bottom --align right --width 5 --height 12 --SetDockType true --SetPartialStrut true --expand true --transparent true --tint 0x000000 &"),
 		((mod1Mask .|. shiftMask, xK_d), spawn "killall trayer"),
